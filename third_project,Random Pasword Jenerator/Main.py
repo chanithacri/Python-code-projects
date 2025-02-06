6e6c1d64c9ff74a24c9cc29343e944e81348f184
chanithacri <113086455+chanithacri@users.noreply.github.com>
2025-02-06 17:00:23 +0000

    Add pasword jenarator project

---
+++ third_project,Random Pasword Jenerator/Main.py
@@ -0,0 +1,3 @@
+# Pasoord should be minmum 8 characters long and should contain at least one uppercase letter, one lowercase letter, one digit and one special character.
+# Ask user for number of characters in password and generate password accordingly(min 8 characters long).
+# Password should be random.
