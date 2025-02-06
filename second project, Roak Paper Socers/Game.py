a9bd2164384e558635cbe6e06e599cd54b23aeb2
chanithacri <113086455+chanithacri@users.noreply.github.com>
2025-02-06 16:49:39 +0000

    chage in code for game.py

--- second project, Roak Paper Socers/Game.py
+++ second project, Roak Paper Socers/Game.py
@@ -1,2 +1,5 @@
+potions = ['rock', 'paper', 'scissors']
 # Make 4 options(human-human, human-easy, human-meadium, human-hard)
 # Note. easy, meadium and hard are the computer
+# Note. hard is optinal
+# Note. hard is the computer that will always win
