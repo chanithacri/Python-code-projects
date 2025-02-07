66c00b95a83850a94c4473a191e7729274aa5a65
chanitha <chanithagunasekera@gmail.com>
2025-02-07 22:34:54 +0530

    Add one part of answer for cal

--- first project, Sicentific cal/Cal.py
+++ first project, Sicentific cal/Cal.py
@@ -1,6 +1,6 @@
 import math
 # If you know how to use a Class
-class cal:
+class Cal:
     def __init__(self, a, b):
         self.num1 = a
         self.num2 = b
@@ -53,10 +53,41 @@ def tan(a):
 
 
 # User input and function calling(You have to compleate this part)
+def class_metord(oparator, num_1, num_2):
+    o = oparator
+    num_1 = num_1
+    num_2 = num_2
+    cal = Cal(num_1, num_2)
+    if o == "+":
+        print(cal.add())
+    elif o == "-":
+        print(cal.sub())
+    elif o == "/":
+        print(cal.div())
+    elif o == "*":
+        print(cal.mul())
+    elif o == "**":
+        print(cal.power())
+    elif o == "%":
+        print(cal.mod())
+    elif o == "squer root":
+        print(cal.root())
+    elif o == "sin":
+        print(cal.sin())
+    elif o == "cos":
+        print(cal.cos())
+    elif o == "tan":
+        print(cal.tan())
+    elif o == "log":
+        print(cal.log())
+    else:
+        print("Invalid oparation try again!!")
+
 # Star like this
 if __name__ == "__main__":
-    print("Enter two numbers: ")
-    a = int(input())
-    b = int(input())
-    print("Enter the operation you want to perform: ")
+    print("Enter two numbers (for cos, sin, tan, log enter only one, sencond one no counted): ")
+    x = int(input("num_1"))
+    y = int(input("num_2"))
+    print("Enter the operation you want to perform(+, -, /, *, %, **, squer root, cos, sin, tan, log): ")
+    oparation = input()
     ...
