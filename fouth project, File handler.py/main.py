3b827e5ac66428a685eb29d37d0bbdbdbcfee6ff
chanithacri <113086455+chanithacri@users.noreply.github.com>
2025-02-07 16:37:16 +0000

    add forut project

---
+++ fouth project, File handler.py/main.py
@@ -0,0 +1,4 @@
+import os
+# Your task is to make a program that can make, deleet, read and write files in a folder.
+#  The nmae, the content and the path of the file should be given by the user true input.
+ 
