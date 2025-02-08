416c8810d960625044ef4afbfd8a744a9886eba5
chanitha <chanithagunasekera@gmail.com>
2025-02-08 21:32:59 +0530

    Started file handler code in main.py

--- fouth project, File handler.py/main.py
+++ fouth project, File handler.py/main.py
@@ -1,4 +1,27 @@
 import os
-# Your task is to make a program that can make, deleet, read and write files in a folder.
-#  The nmae, the content and the path of the file should be given by the user true input.
- 
+
+class FileHandler:
+    def __init__(self):
+        self.folder_name = ''
+        self.file_name = ''
+        self.content = ''
+        self.new_file_permition = False
+
+    def get_forder_name(self):
+        self.folder_name = input('Name the folder>')
+        while os.path.exists(f'{os.path}/{self.folder_name}'):
+            print('Folder exists, do you wand to add file to existing folder. Yes or No >')
+            print(os.listdir(f'{os.path}/{self.folder_name}'))
+            answer = input('>\n')
+            if answer == 'Yes' or 'yes':
+                self.new_file_permition = True
+                break
+            else:
+                self.file_name = input('New folder name> \n')
+                continue
+
+    def make_file(self):
+        if self.new_file_permition:
+            self.file_name = input('Name the file: \n')
+            if os.path.exists(f'{os.path}/{self.file_name}'):
+                answer = input('Do you want to rewrite')
