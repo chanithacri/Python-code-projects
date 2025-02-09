58c3f364f28262375a12a891723b13b1dcbf0fb4
chanithacri <113086455+chanithacri@users.noreply.github.com>
2025-02-09 22:17:08 +0530

    Update main.py
    Signed-off-by: chanithacri <113086455+chanithacri@users.noreply.github.com>

--- fouth project, File handler.py/main.py
+++ fouth project, File handler.py/main.py
@@ -1,4 +1,5 @@
 import os
 # Your task is to make a program that can make, delete, read and write files in a folder.
-#  The nmae, the content and the path of the file should be given by the user true input.
+# You should add options to make new folders or add data to existing folder
+#  The name, the content and the path of the file should be given by the user true input.
  
