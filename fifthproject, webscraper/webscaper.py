dd3434920097d23d5288b67a91a48fb554db0752
chanitha <chanithagunasekera@gmail.com>
2025-02-22 21:17:45 +0530

    ...

---
+++ fifthproject, webscraper/webscaper.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+'''
+https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population
+https://en.wikipedia.org/wiki/List_of_highest-grossing_films
+https://en.wikipedia.org/wiki/List_of_programming_languages
+https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue
+https://en.wikipedia.org/wiki/List_of_tallest_buildings
+'''
+# get the infomation from one of the above wekipedia pages and save the to a file.
