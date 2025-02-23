ae81064b0f0481aa9b05f2d2a84e772d4ce3d9ab
chanitha <chanithagunasekera@gmail.com>
2025-02-23 19:44:59 +0530

    ,

--- fouth project, File handler.py/main.py
+++ fouth project, File handler.py/main.py
@@ -2,3 +2,4 @@ import os
 # Your task is to make a program that can make, delete, read and write files in a folder.
 # You should add options to make new folders or add data to existing folder
 #  Use uer input() to get the requd data from user.
+
